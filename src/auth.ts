06b12e032f11cfd8af21e64076c67ad31b475557
Egnoel <egnoel@hotmail.com>
2026-08-03 19:13:35 +0100

    minor fixes

--- src/auth.ts
+++ src/auth.ts
@@ -6,7 +6,7 @@ import { openAPI } from 'better-auth/plugins'
 
 export const auth = betterAuth({
     basePath: "/auth",
-    trustedOrigins: ["http://localhost:5173"],
+    trustedOrigins: ["http://localhost:5173", "http://127.0.0.1:5173/"],
     database: drizzleAdapter(db, {
         provider: "pg",
         usePlural: true
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import usersRoute from "./routes/users.routes";
 
 const app = new Elysia()
   .use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173/"],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
